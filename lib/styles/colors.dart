72544893f09ba660cd5ebd8d1efbe53c802a6a4b
Moussa ADJI <adjimahamat@gmail.com>
2021-11-29 21:16:31 +0100

    colors class

---
+++ lib/styles/colors.dart
@@ -0,0 +1,16 @@
+/*
+*  colors.dart
+*  Barshave
+*
+*  Created by [Author].
+*  Copyright © 2018 [Company]. All rights reserved.
+    */
+
+import 'dart:ui';
+
+
+class AppColors {
+  static const Color primaryText = Color.fromARGB(255, 0, 0, 0);
+  static const Color secondaryText = Color.fromARGB(255, 138, 138, 143);
+  static const Color accentText = Color.fromARGB(255, 255, 255, 255);
+}
