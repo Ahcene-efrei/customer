bc0c8ad4eb3beb359537e27d18801999d2f6d19c
Ahcene-efrei <62873673+Ahcene-efrei@users.noreply.github.com>
2022-03-06 17:27:42 +0100

    Ajout des couleurs

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -9,7 +9,10 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
-
+const PrimaryColor =  Color.fromARGB(255,197,157,94);
+const SecondaryColor =  Color.fromARGB(255,255, 249, 224);
+const Black =  Color.fromARGB(255,0,0,0);
+const White =  Color.fromARGB(255,255,255,255);
 
 class AppColors {
   static const Color primaryText = Color.fromARGB(255, 0, 0, 0);
@@ -17,8 +20,5 @@ class AppColors {
   static const Color accentText = Color.fromARGB(255, 255, 255, 255);
   static const Color primary = Color.fromARGB(255, 197, 157, 95);
   static const Color yellowStar = Color.fromARGB(255, 255, 142, 1);
-  const PrimaryColor =  Color.fromARGB(255,197,157,94);
-  const SecondaryColor =  Color.fromARGB(255,255, 249, 224);
-  const Black =  Color.fromARGB(255,0,0,0);
-  const White =  Color.fromARGB(255,255,255,255);
+
 }
